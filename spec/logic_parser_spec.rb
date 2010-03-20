f4c307c29218998175ccb264d22ea64f4b2edf53
Bryan Ash <bryan.a.ash@gmail.com>
2010-03-20 01:59:53 -0400

    Refactored specs

--- spec/logic_parser_spec.rb
+++ spec/logic_parser_spec.rb
@@ -9,36 +9,42 @@ describe LogicParser do
 
   describe "parsing a single condition" do
     
+    let(:decision) { parser.parse('Hello') }
+    
     it "identifies the condition" do
-      parser.parse("Hello").condition_identifiers.should == ["Hello"]
+      decision.condition_identifiers.should == ['Hello']
     end
 
     it "conditions is [0], [1]]" do
-      parser.parse("Hello").conditions.should == [[0], [1]]
+      decision.conditions.should == [[0], [1]]
     end
 
   end
 
   describe "parsing 'A or B'" do
 
+    let(:decision) { parser.parse('A or B') }
+    
     it "conditions are ['A','B']" do
-      parser.parse('A or B').condition_identifiers.should == ['A','B']
+      decision.condition_identifiers.should == ['A','B']
     end
 
     it "conditions is [[0,0],[0,1],[1,0],[1,1]]" do
-      parser.parse('A or B').conditions.should == [[0,0],[0,1],[1,0],[1,1]]
+      decision.conditions.should == [[0,0],[0,1],[1,0],[1,1]]
     end
 
   end
 
   describe "parsing 'A and B'" do
 
+    let(:decision) { parser.parse('A and B') }
+    
     it "conditions are ['A','B']" do
-      parser.parse('A and B').condition_identifiers.should == ['A','B']
+      decision.condition_identifiers.should == ['A','B']
     end
 
     it "conditions is [[0,0],[0,1],[1,0],[1,1]]" do
-      parser.parse('A and B').conditions.should == [[0,0],[0,1],[1,0],[1,1]]
+      decision.conditions.should == [[0,0],[0,1],[1,0],[1,1]]
     end
 
   end
