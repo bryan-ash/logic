e51aa802b281aeb0018b54fbdb0ca44f986c631f
Bryan Ash <bryan.a.ash@gmail.com>
2010-03-20 01:05:43 -0400

    parsing an OR

--- spec/logic_parser_spec.rb
+++ spec/logic_parser_spec.rb
@@ -27,4 +27,12 @@ describe LogicParser do
 
   end
 
+  describe "parsing 'A and B'" do
+
+    it "conditions are ['A','B']" do
+      parser.parse('A and B').conditions.should == ['A','B']
+    end
+
+  end
+
 end
