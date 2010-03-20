a396b846b2a24ad1a8808b8324f93d5ab0093219
Bryan Ash <bryan.a.ash@gmail.com>
2010-03-20 01:22:46 -0400

    Refactoring

--- lib/logic_testing.rb
+++ lib/logic_testing.rb
@@ -1,7 +1,35 @@
-module Decision
+module Condition
+  
+  def conditions
+    [text_value]
+  end
 
   def truth_table
     [[0,0], [1,1]]
+  end
+
+end
+
+module Decision
+
+  def conditions
+    elements.map(&:conditions).flatten
+  end
+
+end
+
+module AndStatement
+  
+  def conditions
+    []
+  end
+
+end
+
+module OrStatement
+  
+  def conditions
+    []
   end
 
 end
