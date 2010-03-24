02d5d1caf9a27ba5bef9b937828fa1105165888b
Bryan Ash <bryan.a.ash@gmail.com>
2010-03-23 23:17:20 -0400

    Deciding on output format.  @failing test for multiple condition mcdc_cases

--- logic.rb
+++ logic.rb
@@ -7,6 +7,9 @@ require 'logic_parser'
 parser = LogicParser.new
 logic = parser.parse ARGV[1]
 
+truth_table = TruthTable.new(logic)
+puts truth_table.to_s
+
 conditions = logic.condition_identifiers.join(" ")
 
 puts "#{conditions.rjust(conditions.length + 4)} | output"
