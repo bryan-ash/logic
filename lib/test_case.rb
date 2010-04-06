c45aa39eb820c3f47682f48d33e7283bc4840658
Bryan Ash <bryan.a.ash@gmail.com>
2010-04-05 23:50:09 -0400

    Refactoring

--- lib/test_case.rb
+++ lib/test_case.rb
@@ -2,6 +2,7 @@ require 'integer'
 
 class TestCase
 
+  attr_reader :conditions
   attr_reader :number
   
   def initialize(number, condition_count, decision)
@@ -16,10 +17,6 @@ class TestCase
     @conditions = conditions.dup
   end
 
-  def conditions
-    @conditions
-  end
-
   def create_conditions(condition_count)
     value = number - 1
     value.to_array_of_bits(condition_count)
@@ -29,18 +26,18 @@ class TestCase
     @decision.call(@conditions)
   end
 
-  def negate_condition_at_index!(index)
-    @conditions[index] = @conditions[index].negate
-    self
+  def is_mcdc_case_for_index?(index)
+    modified_case = negate_condition_at_index(index)
+    modified_case.output != output
   end
 
   def negate_condition_at_index(index)
     dup.negate_condition_at_index!(index)
   end
 
-  def is_mcdc_case_for_index?(index)
-    modified_case = negate_condition_at_index(index)
-    modified_case.output != output
+  def negate_condition_at_index!(index)
+    @conditions[index] = @conditions[index].negate
+    self
   end
 
 end
